61ccb160bcccb25db43a3420c380a803114dd3d2
agus monzon <matiasagustinmonzon@gmail.com>
2026-05-15 20:31:33 -0300

    fix css module filenames

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Mongo conectado"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo conectado"))
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Servidor corriendo en puerto 5000");
