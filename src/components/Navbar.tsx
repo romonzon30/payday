f55948cc9ede59092ee1c3ac911afbb334f56dbe
agus monzon <matiasagustinmonzon@gmail.com>
2026-05-21 20:57:09 -0300

    add user menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,32 @@
-import styles from './Navbar.module.css'
+import { useState } from "react";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  function logout() {
+    localStorage.removeItem("token");
+    window.location.reload();
+  }
+
   return (
     <header className={styles.navbar}>
-      <a href="/" className={styles.logo}>
-        PayDay
-      </a>
-      <nav>
-        <a href="/help" className={styles.helpLink}>
-          Help
-        </a>
-      </nav>
+      <div className={styles.logo}>PayDay</div>
+
+      <div className={styles.menuWrapper}>
+        <button className={styles.menuButton} onClick={() => setOpen(!open)}>
+          ☰ Menú
+        </button>
+
+        {open && (
+          <div className={styles.dropdown}>
+            <button>Configuración</button>
+            <button>Datos personales</button>
+            <button>Notificaciones</button>
+            <button onClick={logout}>Cerrar sesión</button>
+          </div>
+        )}
+      </div>
     </header>
-  )
+  );
 }
